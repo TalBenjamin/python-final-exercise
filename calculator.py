cb4a1f560b7cc2778890b5897a4a7c143bab198c
Tal Benjamin <my_email@temp.com>
2024-01-16 10:25:04 +0200

    organized calculator class, changed static methods to non-static

--- calculator.py
+++ calculator.py
@@ -14,26 +14,29 @@ class Calculator:
                  "$": Operator("$", 5, funcs.my_max),
                  "&": Operator("&", 5, funcs.my_min),
                  "%": Operator("%", 4, funcs.modulo),
-                 "~": Operator("~", 6, funcs.neg, is_unary=True, is_pre=True),
+                 "~": Operator("~", 6, funcs.neg, is_unary=True, is_pre=True, is_single=True),
                  "!": Operator("!", 6, funcs.factorial, is_unary=True, is_pre=False),
                  "#": Operator("#", 6, funcs.hashtag, is_unary=True, is_pre=False)
                  }
 
-    @staticmethod
-    def tokenize(expression: str) -> list:
+    def __init__(self):
+        self.__expression = ""
+        self.__token_list = []
+        self.__operands_stk = []
+        self.__operators_stk = []
+
+    def __tokenize(self):
         """
         Breaks expression into it's components (tokens) - operands and operators.
         Also does some basic validity checks on expression(allowed characters, ~, decimal point,
         if operator position makes sense).
-        :param expression: expression to tokenize
         :return: The list of tokens
         :raises SyntaxError if expression isn't a valid mathematical expression
         """
-        tokenized_lst = []
         in_num = False
         cur_num = ""
         last_single = ""  # used to check operators that cant appear more than once
-        for char in expression:
+        for char in self.__expression:
 
             if char == ".":  # take care of decimal point
                 last_single = ""  # nullify this flag
@@ -61,162 +64,154 @@ class Calculator:
                 # if was in number, append to list before continuing
                 if in_num:
                     # check if operand is in ok position
-                    if not Calculator.check_position(tokenized_lst, float(cur_num)):
+                    if not self.__check_position(float(cur_num)):
                         raise SyntaxError("Operand out of place")
-                    tokenized_lst.append(float(cur_num))
+                    self.__token_list.append(float(cur_num))
                     last_single = ""  # nullify this flag
                     cur_num = ""
                     in_num = False
 
                 # if parenthesis, append to list and move to next char
                 if char in Calculator.ALLOWED_PARENTHESIS:
-                    if char == ")" and tokenized_lst and tokenized_lst[-1] == "(":
+                    if char == ")" and self.__token_list and self.__token_list[-1] == "(":
                         raise SyntaxError("Improper use of parenthesis")
-                    tokenized_lst.append(char)
+                    self.__token_list.append(char)
                     continue
-
-                if Calculator.OPERATORS[char].is_single:
-                    last_single = char
 
                 # check that 2 single operators don't appear in a row (like ~)
                 if Calculator.OPERATORS[char].is_single and last_single == char:
                     raise SyntaxError(f"Improper use of operator {char}")
 
-                Calculator.handle_operator_tokenization(tokenized_lst, char)
+                if Calculator.OPERATORS[char].is_single:
+                    last_single = char
+
+                self.__handle_operator_tokenization(char)
 
             else:  # invalid character
                 raise SyntaxError("Invalid character in input")
 
         # if in number, append to list
         if in_num:
-            tokenized_lst.append(float(cur_num))
+            self.__token_list.append(float(cur_num))
 
-        return tokenized_lst
-
-    @staticmethod
-    def handle_operator_tokenization(token_list: list, operator: str):
+    def __handle_operator_tokenization(self, operator: str):
         """
         handles everything related to adding operator to token list.
-        :param token_list: token list
         :param operator: operator to check
         :raises SyntaxError if operator isn't in valid place
         """
         if operator == "~":
-            if (token_list and isinstance(token_list[-1], Operator) and
-                    (token_list[-1].symbol == "~~" or token_list[-1].symbol == "~")):
+            if (self.__token_list and isinstance(self.__token_list[-1], Operator) and
+                    (self.__token_list[-1].symbol == "~~" or self.__token_list[-1].symbol == "~")):
                 raise SyntaxError("Improper use of operator ~")
 
-            # catch unary minus and minus that is part of number (replace with ~)
+        # differentiate unary minus and minus that is part of number (replace with ~)
         if operator == "-":
-            if token_list and isinstance(token_list[-1], Operator):
+            if self.__token_list and isinstance(self.__token_list[-1], Operator):
                 operator = "~"  # minus that is part of operand. max precedence
-            elif not Calculator.is_binary_minus(token_list):
+            elif not self.__is_binary_minus():
                 operator = "~~"  # unary minus
 
-            # get Operator object of char
+        # get Operator object of char
         operator = Calculator.OPERATORS[operator]
 
         # check if operator is in ok place
-        if not operator.can_come_after(token_list[-1] if token_list else None):
+        if not operator.can_come_after(self.__token_list[-1] if self.__token_list else None):
             raise SyntaxError(f"Operator out of place: {operator.symbol}")
 
-        token_list.append(operator)  # append Operator object to list
+        self.__token_list.append(operator)  # append Operator object to list
 
-    @staticmethod
-    def calculate_tokens(expression: list) -> float:
+    def __calculate_tokens(self) -> float:
         """
         calculate expression that list of tokens represents
-        :param expression: expression to be calculated. (token list)
         :return: result of calculation
         :raises SyntaxError, ZeroDivisionError, OverFlowError, ValueError
         """
-        operands_stk = []
-        operators_stk = []
 
-        for element in expression:
+        for element in self.__token_list:
             # if number add to operands stack
             if isinstance(element, float):
-                operands_stk.append(element)
+                self.__operands_stk.append(element)
             elif element == "(":
-                operators_stk.append(element)
+                self.__operators_stk.append(element)
             elif element == ")":
                 # check that ")" doesn't come straight after (
                 # if operators_stk and operators_stk[-1] == "(":
                 #  raise SyntaxError("Invalid use of parenthesis")
 
                 # calculate everything until opening parenthesis
-                while operators_stk and operators_stk[-1] != "(":
-                    Calculator.calc_single(operands_stk, operators_stk)
+                while self.__operators_stk and self.__operators_stk[-1] != "(":
+                    self.__calc_single()
 
                 # if operators stack is empty, it means that there is a missing (
-                if not operators_stk:
+                if not self.__operators_stk:
                     raise SyntaxError("Invalid use of parenthesis")
                 # pop (
-                operators_stk.pop()
+                self.__operators_stk.pop()
             else:  # element is operator
                 # while current operator isn't pre unary, operators stack isn't empty, haven't reached "("
                 # and current operator is weaker than top of stack
-                while not element.is_pre_unary() and operators_stk and operators_stk[-1] != "(" and \
-                        element.precedence <= operators_stk[-1].precedence:
-                    Calculator.calc_single(operands_stk, operators_stk)
+                while not element.is_pre_unary() and self.__operators_stk and self.__operators_stk[-1] != "(" and \
+                        element.precedence <= self.__operators_stk[-1].precedence:
+                    self.__calc_single()
 
                 # append current operator
-                operators_stk.append(element)
+                self.__operators_stk.append(element)
 
         # Perform all remaining operations
-        while operators_stk:
-            Calculator.calc_single(operands_stk, operators_stk)
+        while self.__operators_stk:
+            self.__calc_single()
 
-        if len(operands_stk) != 1:
+        if len(self.__operands_stk) != 1:
             raise SyntaxError("Invalid expression")
 
-        return operands_stk[0]
+        return self.__operands_stk[0]
 
-    @staticmethod
-    def calc_single(operands_stk: list, operators_stk: list):
+    def __calc_single(self):
         """
         performs single operation according to stack statuses
         :return operation result
         :raises SyntaxError, ZeroDivisionError, OverFlowError, ValueError
         """
 
-        operator = operators_stk.pop()
+        operator = self.__operators_stk.pop()
         # Catch ( out of place
         if not isinstance(operator, Operator):
             raise SyntaxError(f"Operator or parenthesis out of place: {operator}")
 
         try:
             if operator.is_unary:
-                op1 = operands_stk.pop()
+                op1 = self.__operands_stk.pop()
                 result = operator.call_func(op1)
             else:
-                op2 = operands_stk.pop()
-                op1 = operands_stk.pop()
+                op2 = self.__operands_stk.pop()
+                op1 = self.__operands_stk.pop()
                 result = operator.call_func(op1, op2)
-            operands_stk.append(result)
+            self.__operands_stk.append(result)
         except IndexError:
             raise SyntaxError(f"Operator or parenthesis out of place: {operator.symbol}")
 
-    @staticmethod
-    def evaluate(expression: str) -> float | int:
+    def evaluate(self, expression: str) -> float | int:
         """
         functions that connects it all. receives a string expression and tries to calculate it
         :return: result of calculation
         :raises SyntaxError, ValueError, ZeroDivisionError, OverflowError
         """
-        expression = "".join(expression.split())  # remove all whitespaces
-        token_lst = Calculator.tokenize(expression)
+        self.__expression = "".join(expression.split())  # remove all whitespaces
+        self.__token_list = []
+        self.__operators_stk = []
+        self.__operands_stk = []
+        self.__tokenize()
         # print(token_lst)
-        result = Calculator.calculate_tokens(token_lst)
+        result = self.__calculate_tokens()
         result = int(result) if result.is_integer() else round(result, 10)
         return result
 
-    @staticmethod
-    def is_binary_minus(token_list: list) -> bool:
+    def __is_binary_minus(self) -> bool:
         # returns true if minus that comes when list is at this state is binary
-        if not token_list:
+        if not self.__token_list:
             return False
-        prev = token_list[-1]
+        prev = self.__token_list[-1]
         if prev == ")" or isinstance(prev, float):
             return True
         if isinstance(prev, Operator):
@@ -232,42 +227,39 @@ class Calculator:
        """
         expression = "".join(expression.split())  # remove all whitespaces
         try:
-            token_lst = Calculator.tokenize(expression)
-            result = Calculator.calculate_tokens(token_lst)
-            result = int(result) if result.is_integer() else round(result, 10)
-            return result
-
+            calc = Calculator()
+            return calc.evaluate(expression)
         except (SyntaxError, ValueError, ZeroDivisionError, OverflowError):
             return "ERROR"
 
-    @staticmethod
-    def check_position(token_list: list, element: str | float) -> bool:
+    def __check_position(self, element: str | float) -> bool:
         """
-        checks if element can come in the end of token_list
-        :param token_list: token list
+        checks if element can come in the end of token_list.
         :param element: element to check. can be str (parenthesis) or float
         :return: true if yes, false otherwise
         """
         if isinstance(element, str):
             if element == "(":
-                if not token_list:
+                if not self.__token_list:
                     return True
-                return isinstance(token_list[-1], Operator) and (not token_list[-1].is_unary or token_list[-1].is_pre)
+                return isinstance(self.__token_list[-1], Operator) and (
+                        not self.__token_list[-1].is_unary or self.__token_list[-1].is_pre)
 
         if isinstance(element, float):
-            if not token_list:
+            if not self.__token_list:
                 return True
-            return token_list[-1] == "(" or (
-                    isinstance(token_list[-1], Operator) and not token_list[-1].is_post_unary())
+            return self.__token_list[-1] == "(" or (
+                    isinstance(self.__token_list[-1], Operator) and not self.__token_list[-1].is_post_unary())
 
         return False
 
 
 def main():
     try:
-        exp = input("Enter math expression: ")
-
-        result = Calculator.evaluate(exp)
+        # exp = input("Enter math expression: ")
+        exp = "2.2 * ~(7.7 - 3.4 @ 2) + 4 ^ (2 $ 5) / 524 % 8"
+        calc = Calculator()
+        result = calc.evaluate(exp)
         print(f"Result is:  {result}")
     except (SyntaxError, ValueError, ZeroDivisionError, OverflowError) as e:
         print(e)
